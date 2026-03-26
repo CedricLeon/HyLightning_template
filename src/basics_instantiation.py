7cf8d13cdb3739ea478c7f6f1284d10c44612c8b
CedricLeon <cedric.leonard@dlr.de>
2026-03-26 11:04:18 +0100

    fix: A wrong check if the model was correctly instantiated. Python dual-module identity problem: isinstance compared  against

--- src/basics_instantiation.py
+++ src/basics_instantiation.py
@@ -1,10 +1,12 @@
 import hydra
-from omegaconf import DictConfig, OmegaConf
-import torch
 
 # Add the root of the project to the python path (more info: https://github.com/ashleve/rootutils)
 import rootutils
+import torch
+from omegaconf import DictConfig, OmegaConf
+
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+
 
 class MyModel:
     def __init__(self, name: str, nb_channels: int, loss: str, lr: float = 0.001):
@@ -12,7 +14,10 @@ class MyModel:
         self.nb_channels = nb_channels
         self.loss = loss
         self.lr = lr
-        print(f"Initialized MyModel with name={name}, nb_channels={nb_channels}, loss={loss}, lr={lr}")
+        print(
+            f"Initialized MyModel with name={name}, nb_channels={nb_channels}, loss={loss}, lr={lr}"
+        )
+
 
 class MyBIGModel:
     def __init__(self, name: str, nb_channels: int, loss: str, optimizer: torch.optim.Optimizer):
@@ -21,7 +26,10 @@ class MyBIGModel:
         self.loss = loss
         self.optimizer = optimizer
 
-        print(f"Initialized MyBIGModel with name={name}, nb_channels={nb_channels}, loss={loss}, optimizer={optimizer.__class__.__name__}")
+        print(
+            f"Initialized MyBIGModel with name={name}, nb_channels={nb_channels}, loss={loss}, optimizer={optimizer.__class__.__name__}"
+        )
+
 
 @hydra.main(version_base=None, config_path="../configs", config_name="basics.yaml")
 def my_app(cfg: DictConfig) -> None:
@@ -30,12 +38,13 @@ def my_app(cfg: DictConfig) -> None:
     print(" --------------------- Model instantiation ----------------- ")
     if cfg.get("model") is not None:
         model = hydra.utils.instantiate(cfg.model)
-        if isinstance(model, (MyModel, MyBIGModel)):
+        if type(model).__name__ in ("MyModel", "MyBIGModel"):
             print(model)
         else:
             print(f"{cfg.model} was not configured correctly.")
     else:
         print("No model specified in the config.")
+
 
 if __name__ == "__main__":
     my_app()
